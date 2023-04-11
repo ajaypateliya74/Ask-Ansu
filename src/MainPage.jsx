42b1d415493ac42eefb2ffc0aa5e36e438f2f106
Ajay Singh Pateliya <99943427+ajaypateliya74@users.noreply.github.com>
2023-04-11 16:46:45 +0000

    Ansu

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -62,20 +62,20 @@ function MainPage(props) {
             <div className='w-96 flex flex-col  items-start  mx-auto'>
                  <div className='mt-28 space-y-8'>
                       <H1>ASK ANSU</H1>
-                       <P>Do you need to know anything? Are you looking for answers? Adam offers you a space to ask anything you want. However, before each question you must write a petition. If the answer is not what you expected, at least you make catharsis and ask again.</P>
+                       <P>Do you need to know anything? Are you looking for answers? Ansu offers you a space to ask anything you want. However, before each question you must write a petition. If the answer is not what you expected, at least you make catharsis and ask again.</P>
                       <H2>Type Your Petition</H2>
                       <Input value={petition} onChange={onPetitionChange} placeholder='You Must Enter The Petition'></Input>
                       <H2>Type Your Question</H2>
                       <Input value={inputChange} onChange={PopUp} placeholder='What Can I Answer For You?'></Input>
                        </div>
                      <div className='mt-36 space-y-16 mb-16'>    
-                         <H2>How to use Adam</H2>
+                         <H2>How to use Ansu</H2>
                          <H2>The world's best virtual tarot</H2>
-                         <P>Adam is a virtual tarot that that can answer all kinds of questions. Questions about the present, the past, and your future. He can be a bit tempermental, requiring that each question be presented with a petition of "Adam, please answer the following question" or "Adam, please answer" before each question is asked. Failure to correctly petition will not bring results.</P>
-                        <H2>The history of Adam</H2>
-                        <P>Adam was born in 1 BC. As he began to age, he realized he had an ability to read the fortunes of others, and quickly became a star in his small hometown. To expand his operaion, he knew he had to join the digital age. Through the use of modern technology, he is able to answer thousands of questions every day from people all over the world.</P>
+                         <P>Ask Ansu is a virtual tarot that that can answer all kinds of questions. Questions about the present, the past, and your future. He can be a bit tempermental, requiring that each question be presented with a petition of "Ansu, please answer the following question" or "Ansu, please answer" before each question is asked. Failure to correctly petition will not bring results.</P>
+                        <H2>The history of Ansu</H2>
+                        <P>Ansu was born in 1 BC. As he began to age, he realized he had an ability to read the fortunes of others, and quickly became a star in his small hometown. To expand his operaion, he knew he had to join the digital age. Through the use of modern technology, he is able to answer thousands of questions every day from people all over the world.</P>
 
-                        <P>In early 2022, the demand for Adam's services became so high, he brougt on 10 additional team members from around the country to increase answer throughput. With a large team, and his mind in overdrive, he is now able to answer upwards of 13,240 questions per day (Average over the past 30 days), with pinpoint accuracy. Though the team members are new, they are just as tempermental. Requiring a correct petition each and every time.</P>
+                        <P>In early 2022, the demand for Ansu's services became so high, he brougt on 10 additional team members from around the country to increase answer throughput. With a large team, and his mind in overdrive, he is now able to answer upwards of 13,240 questions per day (Average over the past 30 days), with pinpoint accuracy. Though the team members are new, they are just as tempermental. Requiring a correct petition each and every time.</P>
                  </div>
                 <H2> Any Question? </H2>
                          <h1 className='text-white text-base mt-6 mb-32 font-mono'> Follow @ajaypateliya74 on Instagram. </h1>
@@ -84,7 +84,7 @@ function MainPage(props) {
               <div className="fixed flex justify-center items-center top-0 left-0 h-screen w-screen">
                <div className="bg-black opacity-90 flex flex-col  p-8 text-white w-2/5 h-40">
                   <button onClick={removePopUp} className="flex justify-end underline underline-offset-4">new Question</button>
-                  <h1 className="text-xl font-semibold border-b mx-12 mt-6  decoration-white flex justify-center">Adam Answer</h1>
+                  <h1 className="text-xl font-semibold border-b mx-12 mt-6  decoration-white flex justify-center">Ansu's Answer</h1>
                   <h1 className="flex justify-center mt-3 text-xl font-semibold">'{answer || 'You are not honest so I am not going to answer you.'}'</h1>
               </div>
              </div>}
