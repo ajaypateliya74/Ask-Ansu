30d1f2f2197a968cbb58a31d1226ac7a6a869d8c
ajaypateliya74 <99943427+ajaypateliya74@users.noreply.github.com>
2023-07-18 02:00:45 +0000

    Setup gtag for nav bar clicking track

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -3,6 +3,7 @@ import Input from './Input.jsx';
 import H2 from './H2.jsx';
 import H1 from './H1.jsx';
 import P from './p.jsx';
+import MainNav from './mainNav.jsx';
 
 
 function MainPage(props) {
@@ -135,7 +136,10 @@ function MainPage(props) {
     return (
         <>
             <div className='bg-black  h-full w-full'>
+                <MainNav></MainNav>
+
                 <div className='w-96 flex flex-col  items-start  mx-auto'>
+
                     <div className='mt-28 space-y-8'>
                         <H1>ASK ANSU</H1>
                         <P>Do you need to know anything? Are you looking for answers? Ansu offers you a space to ask anything you want. However, before each question you must write a petition. If the answer is not what you expected, at least you make catharsis and ask again.</P>
@@ -155,7 +159,8 @@ function MainPage(props) {
                         <P>In early 2022, the demand for Ansu's services became so high, he brougt on 10 additional team members from around the country to increase answer throughput. With a large team, and his mind in overdrive, he is now able to answer upwards of 13,240 questions per day (Average over the past 30 days), with pinpoint accuracy. Though the team members are new, they are just as tempermental. Requiring a correct petition each and every time.</P>
                     </div>
                     <H2> Any Question? </H2>
-                    <button onClick={openPopUpForm} id="setBtn" className="absolute top-4 text-white  right-4">Join Now</button>
+                    <button onClick={openPopUpForm} id="setBtn" className="p-8 text-white font-bold text-2xl absolute top-20 right-2">Join Now</button>
+
                     <h1 className='text-white text-base mt-6 mb-32 font-mono'> Follow @ajaypateliya74 on Instagram. </h1>
                 </div>
                 {popUp &&
---
+++ src/mainNav.jsx
@@ -0,0 +1,163 @@
+import React, { useState, useEffect } from 'react';
+import './MainPage.css';
+
+function MainNav(props) {
+    const [activeDropdown, setActiveDropdown] = useState(null);
+
+    const handleDropdownOpen = (heading) => {
+        setActiveDropdown(heading);
+    };
+
+    const handleDropdownClose = () => {
+        setActiveDropdown(null);
+    };
+
+    const handleClick = (heading, subHeading) => {
+        if (activeDropdown === heading) {
+            setActiveDropdown(null);
+        } else {
+            setActiveDropdown(heading);
+        }
+
+        if (subHeading) {
+            console.log(heading);
+            console.log(subHeading);
+
+
+            window.gtag('event', 'navbar_click', {
+                event_label: 'Navbar',
+                clicked_heading: heading,
+                clicked_sub_heading: subHeading
+            });
+        }
+    };
+
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+            if (activeDropdown && !event.target.closest('.custom-width')) {
+                setActiveDropdown(null);
+            }
+        };
+
+        document.addEventListener('click', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('click', handleOutsideClick);
+        };
+    }, [activeDropdown]);
+
+    return (
+        <nav className="bg-white py-8 fixed w-full">
+            <ul className="flex space-x-72 font-bold text-lg justify-center">
+                <li
+                    className="text-black relative"
+                    onClick={() => handleClick('HOME', '')}
+                >
+                    <span>HOME</span>
+                    {activeDropdown === 'HOME' && (
+                        <ul className="absolute -left-32 top-12 p-32 bg-white shadow-lg py-4 font-bold space-y-8">
+                            <li
+                                className="custom-width text-black cursor-pointer"
+                                onClick={() => handleClick('HOME', 'ASK ME')}
+                            >
+                                ASK ME
+                            </li>
+                            <li
+                                className="text-black custom-width cursor-pointer"
+                                onClick={() => handleClick('HOME', 'ASK ANSU')}
+                            >
+                                ASK ANSU
+                            </li>
+                            <li
+                                className="text-black custom-width cursor-pointer"
+                                onClick={() => handleClick('HOME', 'ASK ADAM')}
+                            >
+                                ASK ADAM
+                            </li>
+                        </ul>
+                    )}
+                </li>
+                <li
+                    className="text-black relative"
+                    onClick={() => handleClick('ABOUT', '')}
+                >
+                    <span>ABOUT</span>
+                    {activeDropdown === 'ABOUT' && (
+                        <ul className="absolute -left-32 top-12 p-32 bg-white shadow-lg py-4 font-bold space-y-8">
+                            <li
+                                className="text-black custom-width cursor-pointer"
+                                onClick={() => handleClick('ABOUT', 'About ME')}
+                            >
+                                ABOUT ME
+                            </li>
+                            <li
+                                className="text-black custom-width cursor-pointer"
+                                onClick={() => handleClick('ABOUT', 'About ANSU')}
+                            >
+                                ABOUT ANSU
+                            </li>
+                        </ul>
+                    )}
+                </li>
+                <li
+                    className="text-black relative"
+                    onClick={() => handleClick('CONTACT', '')}
+                >
+                    <span>CONTACT</span>
+                    {activeDropdown === 'CONTACT' && (
+                        <ul className="absolute -left-32 top-12 p-32 bg-white shadow-lg py-4 font-bold space-y-8">
+                            <li
+                                className="text-black custom-width cursor-pointer"
+                                onClick={() => handleClick('CONTACT', 'EMAIL ME')}
+                            >
+                                EMAIL ME
+                            </li>
+                            <li
+                                className="text-black custom-width cursor-pointer"
+                                onClick={() => handleClick('CONTACT', 'CALL ME')}
+                            >
+                                CALL ME
+                            </li>
+                            <li
+                                className="text-black custom-width cursor-pointer"
+                                onClick={() => handleClick('CONTACT', 'FOLLOW ME')}
+                            >
+                                FOLLOW ME
+                            </li>
+                            <li
+                                className="text-black custom-width cursor-pointer"
+                                onClick={() => handleClick('CONTACT', 'SLACK ME')}
+                            >
+                                SLACK ME
+                            </li>
+                        </ul>
+                    )}
+                </li>
+                <li
+                    className="text-black relative"
+                    onClick={() => handleClick('PREMIUM', '')}
+                >
+                    <span>PREMIUM</span>
+                    {activeDropdown === 'PREMIUM' && (
+                        <ul className="absolute -left-32 top-12 p-32 bg-white shadow-lg py-4 font-bold space-y-8">
+                            <li
+                                className="text-black custom-width cursor-pointer"
+                                onClick={() => handleClick('PREMIUM', 'SILVER PREMIUM')}
+                            >
+                                SILVER PREMIUM
+                            </li>
+                            <li
+                                className="text-black custom-width cursor-pointer"
+                                onClick={() => handleClick('PREMIUM', 'GOLD PREMIUM')}
+                            >
+                                GOLD PREMIUM
+                            </li>
+                        </ul>
+                    )}
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default MainNav;
